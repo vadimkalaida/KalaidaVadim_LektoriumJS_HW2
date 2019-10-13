25190e38d5d60b2ad8ddbdde799396f4fa29cd61
VadimKalayda <vadosgta1@gmail.com>
2019-10-13 20:40:45 +0300

    task1 - ready

--- Task1/index.js
+++ Task1/index.js
@@ -1,5 +1,6 @@
 let myArray = [3,0,-5,NaN,1,44,null, undefined, 'hello',-12,3,0,0,1,2,-3,-3,2,1,4,-2,-3,-1];
 let numberArray = [];
+let resultsArray = [];
 
 function makeCorrectArray(arr) {
   arr.forEach(function (i) {
@@ -36,8 +37,11 @@ let mySum = numberArray.reduce(function(a, b) {
 });
 
 console.log('Max = ', myMax);
+resultsArray.push(myMax);
 console.log('Min = ', myMin);
+resultsArray.push(myMin);
 console.log('Sum = ', mySum);
+resultsArray.push(mySum);
 
 console.log('Замінив деякі елемнети в массиві')
 
@@ -65,7 +69,19 @@ let mySum2 = numberArray.reduce(function(a, b) {
 });
 
 console.log('Max = ', myMax2);
+resultsArray.push(myMax2);
 console.log('Min = ', myMin2);
+resultsArray.push(myMin2);
 console.log('Sum = ', mySum2);
+resultsArray.push(mySum2);
 
 let myObj = {};
+
+function resultsArrayToObject(arr, obj) {
+  for(let i = 0; i < arr.length; i++) {
+    obj[i] = arr[i];
+  }
+}
+
+resultsArrayToObject(resultsArray, myObj);
+console.log('Об\'єкт з результатами: ', myObj);
