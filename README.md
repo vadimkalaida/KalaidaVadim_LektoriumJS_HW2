55a774ab5d9e0e8b0e57b1b4a093af1ae59c48fa
VadimKalayda <vadosgta1@gmail.com>
2019-10-15 23:44:24 +0300

    made banknotes change

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 KalaidaVadim_LektoriumJS_HW2(NOT READY)
 - TASK 1 - READY
 - TASK 2 - READY
-- TASK 3 - NOT READY
-- TASK 4 - NOT READY
+- TASK 3 - READY(incompletely)
+- TASK 4 - READY
--- Task3/index.js
+++ Task3/index.js
@@ -1,7 +1,10 @@
 let price = parseInt(prompt('Price:')),
   banknote = parseInt(prompt('Banknote(example: 50):')),
+  allBanknotes = [1, 2, 5, 10, 20, 50, 100, 200, 500],
+  changeBanknotes = [],
   priceInput = document.querySelector('#priceInput'),
-  banknoteInput = document.querySelector('#banknoteInput');
+  banknoteInput = document.querySelector('#banknoteInput'),
+  changeInput = document.querySelector('#changeInput');
 
 if(isNaN(price) || price <= 0) {
   alert('Price/Banknote must be >= 0!!!');
@@ -18,5 +21,19 @@ if(price > banknote) {
   location.reload();
 }
 
+let change = banknote - price;
+
 priceInput.value = price;
 banknoteInput.value = banknote;
+changeInput.value = change;
+
+for(let i = allBanknotes.length - 1; i >= 0; i--) {
+  while((change - allBanknotes[i]) >= 0) {
+    change -= allBanknotes[i];
+    changeBanknotes.push(allBanknotes[i]);
+  }
+}
+
+document.querySelector('#changeBanknotesInput').value = changeBanknotes;
+
+console.log(changeBanknotes);
